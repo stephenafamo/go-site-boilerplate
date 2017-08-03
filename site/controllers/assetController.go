875033314b1fddd046472d8b1515af3f57d3600e
Stephen Afam-Osemene <stephenafamo@gmail.com>
2017-08-03 10:05:43 +0100

    Fix bug in AssetController

--- site/controllers/assetController.go
+++ site/controllers/assetController.go
@@ -3,14 +3,15 @@ package controller
 import (
 	"net/http"
 	"path"
+	"github.com/stephenafamo/what-to-do/config"
 )
 
 type AssetController struct {
 	Controller
 }
 
-func (a *AssetController) Index(w http.ResponseWriter, r *http.Request, p interface{}) {
-	AssetBaseFolder := "/go/src/github.com/stephenafamo/site/assets"
+func (a *AssetController) Index(w http.ResponseWriter, r *http.Request, p interface{}) {	
+	AssetBaseFolder := config.GetS("AssetBaseFolder")
 	vars := a.GetVars(r)
 	if _, ok := vars["path"]; ok != true{
 		http.NotFound(w, r )
